0cdf64a81a9ac17841063b073047f82c2e7d0d19
eunsxx <88652027+eunsxx@users.noreply.github.com>
2024-05-30 01:01:50 +0900

    7, 8주차 워크북 업로드

---
+++ src/main/java/umc/spring/apiPayload/code/ErrorReasonDTO.java
@@ -0,0 +1,18 @@
+package umc.spring.apiPayload.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorReasonDTO {
+    private String message;  // 오류 메시지
+    private String code;     // 오류 코드
+    private boolean isSuccess; // API 호출 성공 여부
+    private HttpStatus httpStatus; // HTTP 상태 코드
+}
